8ec8921a8659032db3ff0ece107f0742d245bcbd
emekadefirst <victorchibuogwu33@gmail.com>
2025-04-14 14:54:06 +0100

    POST and GET

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const apiDomain = 'https://pale-joellen-emekadefirst-db64cc83.koyeb.app/';
+const apiDomain = 'https://pale-joellen-emekadefirst-db64cc83.koyeb.app';
 // const apiDomain = 'http://127.0.0.1:8000';
 // window.REACT_APP_API_DOMAIN ||
 
